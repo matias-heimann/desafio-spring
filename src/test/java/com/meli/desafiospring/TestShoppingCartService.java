457444baac31ac24971dbede596f6e4aa87be979
Matias Adriel Heimann <matias.heimann@mercadolibre.com>
2021-03-15 16:25:25 -0300

    shopping cart service

--- src/test/java/com/meli/desafiospring/TestShoppingCartService.java
+++ src/test/java/com/meli/desafiospring/TestShoppingCartService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.meli.desafiospring.exceptions.FilterNotValidException;
 import com.meli.desafiospring.exceptions.NotEnoughProductsException;
 import com.meli.desafiospring.exceptions.NotFoundProductException;
+import com.meli.desafiospring.exceptions.NotShoppingCartOpen;
 import com.meli.desafiospring.model.IndividualPurchase;
 import com.meli.desafiospring.model.ProductDAO;
 import com.meli.desafiospring.model.PurchaseArticles;
@@ -121,5 +122,36 @@ public class TestShoppingCartService {
                 new StatusCodeDTO(HttpStatus.OK, "The products were added successfully"));
 
         Assert.assertEquals(expected, this.shoppingCartService.addItemsToList(purchaseArticles));
+    }
+
+    @Test
+    public void testCloseCart() throws FilterNotValidException, NotFoundProductException, NotEnoughProductsException, IOException, NotShoppingCartOpen {
+        this.testAddItemToCart();
+
+        List<IndividualPurchase> purchases = new LinkedList<>();
+        purchases.add(new IndividualPurchase(0, 1));
+        PurchaseArticles purchaseArticles = new PurchaseArticles(purchases);
+
+        Set<Integer> ids = new HashSet<>();
+        ids.add(0);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<ProductDAO> productDAOS = objectMapper.readValue(new File(ONE_ID_FILE),
+                new TypeReference<>(){});
+
+        List<ProductTicketDTO> productTicketDTOS = new LinkedList<>();
+        productTicketDTOS.add(new ProductTicketDTO(0, "Desmalezadora", "Makita", 1));
+
+        Mockito.when(this.shoppingRepository.getOpenShoppingCart()).thenReturn(new ShoppingCartDao(0, purchases));
+        Mockito.when(this.productRepository.getByIds(ids)).thenReturn(productDAOS);
+
+        BuyOrderDTO expected = new BuyOrderDTO(0, productTicketDTOS, 9600,
+                new StatusCodeDTO(HttpStatus.OK, "The purchase was done successfully"));
+        Assert.assertEquals(expected, this.shoppingCartService.finishBuyOrder());
+    }
+
+    public void testCloseShoppingCartWhenThereIsNoCartOpen(){
+        Mockito.when(this.shoppingRepository.getOpenShoppingCart()).thenReturn(null);
+        Assert.assertThrows(NotShoppingCartOpen.class, () -> this.shoppingCartService.finishBuyOrder());
     }
 }
